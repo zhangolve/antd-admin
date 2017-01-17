3988628df1ec9f9bf48cbe87dd739a87587bdb77
zuiidea <zuiiidea@gmail.com>
2017-01-17 21:39:34 +0800

    'roadhog'

---
+++ src/index1.js
@@ -0,0 +1,17 @@
+import dva from 'dva'
+import './index.html'
+
+// 1. Initialize
+const app = dva()
+
+// 2. Plugins
+// app.use(createLoading())
+
+// 3. Model
+app.model(require('./models/users'))
+
+// 4. Router
+app.router(require('./router'))
+
+// 5. Start
+app.start('#root')
